002a10580b7779bd24573ec4fe8fe232240631c1
heriawanfx <heriawan.fx@gmail.com>
2021-10-02 13:57:41 +0700

    Add Encription n Pinning

--- core/src/main/java/com/heriawanfx/restaurant/core/di/CoreModule.kt
+++ core/src/main/java/com/heriawanfx/restaurant/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.heriawanfx.restaurant.core.data.source.remote.RemoteDataSource
 import com.heriawanfx.restaurant.core.data.source.remote.network.ApiService
 import com.heriawanfx.restaurant.core.domain.repository.IRestaurantRepository
 import com.heriawanfx.restaurant.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,31 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<RestaurantDatabase>().restaurantDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("heriawanfx".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             RestaurantDatabase::class.java, "Restaurant.db"
-        ).fallbackToDestructiveMigration().build()
+        )
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "restaurant-api.dicoding.dev"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/hWR325mqh8kIxVsVJnfJGAsD+T5rjcqZU2UsBlVf4Qw=")
+            .add(hostname, "sha256/cXjPgKdVe6iojP8s0YQJ3rtmDFHTnYZxcYvmYGFiYME=")
+            .add(hostname, "sha256/hxqRlPTu1bMS/0DITB1SSu0vd4u/8l8TjPgfaAp63Gc=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
